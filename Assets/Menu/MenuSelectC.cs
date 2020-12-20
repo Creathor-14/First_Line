81b1b03a73c17188b3c4348b86c73ffdc7def178
PePe-14 <73206031+PePe-14@users.noreply.github.com>
2020-12-20 16:59:27 -0300

    MenuAvance

--- Assets/Menu/MenuSelectC.cs
+++ Assets/Menu/MenuSelectC.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuPrincipal : MonoBehaviour
+public class MenuSelectC : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
