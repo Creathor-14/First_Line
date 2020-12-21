e51055cf07d647e4ecfd3c61d218a2465fd2aca6
ElAdminExe <thomas.quiroga@alumnos.ucn>
2020-12-20 18:19:05 -0600

    codigo movimiento mejorado
    codigo:
    +movimiento
    +flip
    +caminar

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent(typeof(SpriteRenderer))]
-    [RequireComponent(typeof(Animator))]
 
 public class PlayerMove : MonoBehaviour
 {
-    private SpriteRenderer sprite;
     public float speed= 4f;
 
-    private void Awake()
-    {
-        sprite = GetComponent<SpriteRenderer>();
-    }
+    private Rigidbody2D rb;
+
+    private SpriteRenderer player;
+
+    private Animator caminar;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<SpriteRenderer>();
+        caminar = GetComponent<Animator>();
     }
 
 
@@ -26,14 +26,21 @@ public class PlayerMove : MonoBehaviour
         Vector3 mov = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
         transform.position = Vector3.MoveTowards(transform.position, transform.position + mov, Time.deltaTime * speed);
         
-        
+        if (mov.x==0 && mov.y==0)
+        {
+            caminar.SetBool("Caminar",false);
+        }
+        else
+        {
+            caminar.SetBool("Caminar",true);
+        }
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            sprite.flipX = true;
+            player.flipX = true;
         }
         else if(Input.GetAxisRaw("Horizontal") >0)
         {
-            sprite.flipX = false;
+            player.flipX = false;
         }    
         
     }
