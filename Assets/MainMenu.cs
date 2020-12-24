8aa746ac553704c62484291557e7f9d02311dc14
PePe-14 <73206031+PePe-14@users.noreply.github.com>
2020-12-24 03:53:59 -0300

    EscenasMenu y sprites separados

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayerSelect()
+    {
+        SceneManager.LoadScene("PlayerSelection");
+    }
+
+    public void StarGame()
+    {
+        SceneManager.LoadScene("Golpes");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("LoadGame");
+    }
+    
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    
+    public void Tips()
+    {
+        SceneManager.LoadScene("Tips");
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Ha salido del juego");
+    }
+}
