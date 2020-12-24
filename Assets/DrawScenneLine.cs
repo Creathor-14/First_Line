00c0b1235456597bd68679eca9550e9f939aa695
Cristobal Ríos <cristobalrios2001@gmail.com>
2020-12-24 00:50:45 -0300

    Mapa 1 y Camara

---
+++ Assets/DrawScenneLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawScenneLine : MonoBehaviour
+{
+    public Transform from;
+    public Transform to;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (from != null && to != null)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(@from.position,to.position);
+            Gizmos.DrawSphere(@from.position, 0.40f);
+            Gizmos.DrawSphere(@to.position, 0.40f);
+        }
+    }
+}
---
+++ Assets/plataformaMovil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaMovil : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target != null)
+        {
+            target.parent = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (target != null)
+        {
+            float fixedSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, fixedSpeed);
+        }
+    }
+}
