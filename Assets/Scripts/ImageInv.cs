c2a5c10546b59fda775ed816ed5f4d32f4a1a87c
ElAdminExe <thomas.quiroga@alumnos.ucn>
2021-01-03 17:40:57 -0600

    Pal que quiera tomar el reto

---
+++ Assets/Scripts/ImageInv.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageInv : MonoBehaviour
+{
+    public Image image;
+
+    private int cont = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cont == 0)//aparece la imagen
+        {
+            image.color = new Color(255,255,0,255); 
+        }
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            image.color = new Color(255,255,255,27);//desvanece la imagen
+            cont += 1;
+            print("Fire1");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            cont = 0;
+            print("Fire2");
+        }
+    }
+
+    
+}
