085abc9f15e5184c35e2d64d63500d267ae11bd5
Cristobal Ríos <cristobalrios2001@gmail.com>
2021-01-13 16:18:48 -0300

    ahora si AA

---
+++ Assets/Iluminacion_Global.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
+using Random=UnityEngine.Random;
+
+public class Iluminacion_Global : MonoBehaviour
+{
+    
+    Light2D myLight;
+
+    public Image BarraVida;
+    
+
+    private void Start()
+    {
+        myLight = GetComponent<Light2D>();
+        myLight.intensity = BarraVida.fillAmount;
+        
+    
+    }
+
+    
+}
