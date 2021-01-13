2a8d070402a702bfe37bcc4125847d24f6d4e9e0
Cristobal Ríos <cristobalrios2001@gmail.com>
2021-01-13 15:45:34 -0300

    aa

--- Assets/LucesIntermitentes.cs
+++ Assets/LucesIntermitentes.cs
@@ -27,15 +27,16 @@ public class LucesIntermitentes : MonoBehaviour
 
     IEnumerator StartScene()
     {
-        yield return new WaitForSeconds(beginningTime);
+        yield return new WaitForSeconds(beginningTime);  // variable tiempo de parpadeo
         StartCoroutine(LightFlicker());
     }
 
     IEnumerator LightFlicker()
     {
-        yield return new WaitForSeconds(betweenLightFlickers);
-        //myLight.pointLightOuterRadius = Random.Range(lightFlickerMin,lightFlickerMax);
-        myLight.pointLightOuterRadius = BarraVida.fillAmount*lightFlickerMin;
+        yield return new WaitForSeconds(betweenLightFlickers); // 
+        myLight.pointLightOuterRadius = Random.Range(lightFlickerMin,lightFlickerMax);  // El random determinara el radio de iluminacion de la luz
+        //myLight.pointLightOuterRadius = BarraVida.fillAmount*lightFlickerMin; 
+        myLight.intensity = BarraVida.fillAmount; // se llama a la barra de vida y con el valor del fillamount que se obtenga, variara la intensidad de la luz
         StartCoroutine(LightFlicker());
     }
 }
