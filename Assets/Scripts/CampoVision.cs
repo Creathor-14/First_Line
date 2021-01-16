2f0fe066b12d95a6cacbbfa81897c2727a537579
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-01-16 17:58:33 -0300

    DrakeG(Eficiencia)
    Tomy qlo

---
+++ Assets/Scripts/CampoVision.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampoVision : MonoBehaviour
+{
+    public float ViewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask AtaqueMask;
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    private void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+    
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, ViewRadius, AtaqueMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, distToTarget, AtaqueMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees*Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad ));
+    }
+    
+    
+    
+}
---
+++ Assets/Scripts/Editor/CampoVisionEditor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CampoVision))]
+public class CampoVisionEditor : Editor 
+{
+    void OnSceneGUI()
+    {
+        CampoVision cv = (CampoVision)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(cv.transform.position, Vector3.up, Vector3.forward, 360,cv.ViewRadius);
+        Vector3 viewAngleA = cv.DirFromAngle(-cv.viewAngle / 2, false);
+        Vector3 viewAngleB = cv.DirFromAngle(cv.viewAngle / 2, false);
+        
+        Handles.DrawLine(cv.transform.position, cv.transform.position+viewAngleA*cv.ViewRadius);
+        Handles.DrawLine(cv.transform.position, cv.transform.position+viewAngleB*cv.ViewRadius);
+        
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in cv.visibleTargets)
+        {
+            Handles.DrawLine(cv.transform.position, visibleTarget.position);
+        }
+        {
+            
+        }
+
+    }
+}
