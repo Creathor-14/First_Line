66d4161e71440438c5c77e413a595214a37c05d0
Cristobal Ríos <cristobalrios2001@gmail.com>
2021-01-21 03:26:52 -0300

    Cambios e Implementacion Maps
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/Levels/Level_1/plataformaMovil.cs
+++ Assets/Levels/Level_1/plataformaMovil.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 public class plataformaMovil : MonoBehaviour
 {
     public Transform target;
-
     public float speed;
-    // Start is called before the first frame update
+
+    //private Vector3 start, end;
     void Start()
     {
         if (target != null)
         {
             target.parent = null;
+            /*
+            start = transform.position; // toma la posicion inicial de la plataforma
+            end = target.position; // toma la posicion del "target"
+            */
         }
     }
     
@@ -25,5 +29,12 @@ public class plataformaMovil : MonoBehaviour
             float fixedSpeed = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target.position, fixedSpeed);
         }
+        
+        /*
+        if (transform.position == target.position)
+        {
+            target.position = (target.position == start) ? end : start; // comprueba la posicion del target, se verifica si esta en start. Si no lo esta target.position == start, else target.position == end
+        }
+        */
     }
 }
